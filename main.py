39ad34efeac94611a404f50550464022be13a979
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-05-13 01:43:50 +0530

    Refactor upload folder path definition
    Updated the upload folder path to use os.path.join for better compatibility.

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER=os.path.join(app.root_path,"uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-
     if request.method == 'POST':
-
         file = request.files['file']
-
         if file.filename != '':
             file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-
     return render_template('index.html')
 
 app.run(host='0.0.0.0', port=5000)
