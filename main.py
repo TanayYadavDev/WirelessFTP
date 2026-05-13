3b663ac1e196a6cea7b418306217e10160dbffbd
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-05-13 12:01:17 +0530

    Implement QR code generation for server IP
    Added QR code generation for server IP and created static folder for saving the QR code image.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory
+import qrcode, socket
 import os
 
 app = Flask(__name__)
@@ -6,7 +7,8 @@ app = Flask(__name__)
 UPLOAD_FOLDER=os.path.join(app.root_path,"uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-files=os.listdir(UPLOAD_FOLDER)
+STATIC_FOLDER=os.path.join(app.root_path,"static")
+os.makedirs(STATIC_FOLDER,exist_ok=True)
 
 @app.route('/download/<filename>')
 def download_file(filename):
@@ -19,11 +21,26 @@ def download_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     user_ip = request.remote_addr
+    
     if request.method == 'POST':
         file = request.files['file']
         if file.filename != '':
             file.save(os.path.join(UPLOAD_FOLDER, file.filename))
     files=os.listdir(UPLOAD_FOLDER)
+    
+    s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    server_ip=s.getsockname()[0]
+    s.close()
+    
+    qr=qrcode.make("http://"+server_ip+":5000")
+    qr.save(
+        os.path.join(
+            STATIC_FOLDER,
+            "qrcode.png"
+        )
+    )
     return render_template('index.html', ip=user_ip, files=files)
 
+    
 app.run(host='0.0.0.0', port=5000)
